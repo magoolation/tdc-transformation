26ee9f5472f11776de7e096e1baef8429b44e489
Alexandre Costa <alexandresantoscosta@gmail.com>
2021-08-26 11:55:29 -0300

    Added analytics to track most important events

--- XamarinDemo/MainPage.xaml.cs
+++ XamarinDemo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using System;
 using System.Collections.Generic;
@@ -14,12 +15,29 @@ namespace XamarinDemo
     {
         private Random rnd = new Random();
         private int _number;
+        private  int _tries = 0;
 
         public MainPage()
         {
             InitializeComponent();
 
+            StartNewGame();
+            NewGame.IsEnabled = false;
+        }
+
+        private void StartNewGame()
+        {
+            _tries = 0;
             _number = 1 + rnd.Next(99);
+
+            Analytics.TrackEvent("New Game", new Dictionary<string, string>()
+            {
+                { "Value", _number.ToString() }
+            });
+
+            Submit.IsEnabled = true;
+            Guess.Text = "";
+            Guess.Focus();
             NewGame.IsEnabled = false;
         }
 
@@ -30,18 +48,32 @@ namespace XamarinDemo
 
         private void Guess_Button_Clicked(object sender, EventArgs e)
         {
+            _tries++;
+
             try
             {
                 int guess = int.Parse(Guess.Text);
 
                 if (guess == _number)
                 {
+                    Analytics.TrackEvent("Right guess", new Dictionary<string, string>()
+                    {
+                        { "Guess", Guess.Text },
+                        {                         "Tries", _tries.ToString() },
+                    });
+
                     DisplayAlert("Success", "You got it!", "OK");
                     Submit.IsEnabled = false;
                     NewGame.IsEnabled = true;
                 }
                 else
-                {
+                { 
+                    Analytics.TrackEvent("Wrong Guess", new Dictionary<string, string>()
+                    {
+                        { "Guess", Guess.Text },
+                        { "Tries", _tries.ToString() },
+                    });
+
                     DisplayAlert("Oops", "Not this time! Please try again!", "OK");
                     Guess.Text = "";
                     Guess.Focus();
@@ -61,11 +93,7 @@ namespace XamarinDemo
 
         private void NewGame_Button_Clicked(object sender, EventArgs e)
         {
-            _number = 1 + rnd.Next(99);
-            Submit.IsEnabled = true;
-            Guess.Text = "";
-            Guess.Focus();
-            NewGame.IsEnabled = false;
+            StartNewGame();
         }
     }
 }

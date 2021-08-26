9047a5b9400bbcd67876332fcd5ce7c4d2cd02f9
Alexandre Costa <alexandresantoscosta@gmail.com>
2021-08-26 11:20:55 -0300

    Implemented business logic

--- ReadMe.md
+++ ReadMe.md
@@ -4,4 +4,4 @@ Demo for the TDC Transformation 2021.
 
 ## # Build Status
 
-[[Build Status](https://build.appcenter.ms/v0.1/apps/4201e6b3-e4b4-4ccb-ae08-2dd0170e386e/branches/main/badge)] https://build.appcenter.ms/v0.1/apps/4201e6b3-e4b4-4ccb-ae08-2dd0170e386e/branches/main/badge
+[Build Status](https://build.appcenter.ms/v0.1/apps/4201e6b3-e4b4-4ccb-ae08-2dd0170e386e/branches/main/badge) https://build.appcenter.ms/v0.1/apps/4201e6b3-e4b4-4ccb-ae08-2dd0170e386e/branches/main/badge)
--- XamarinDemo/MainPage.xaml.cs
+++ XamarinDemo/MainPage.xaml.cs
@@ -12,14 +12,47 @@ namespace XamarinDemo
 {
     public partial class MainPage : ContentPage
     {
+        private Random rnd = new Random();
+        private int _number;
+
         public MainPage()
         {
             InitializeComponent();
+
+            _number = 1 + rnd.Next(99);
+            NewGame.IsEnabled = false;
         }
 
         private void Crash_Button_Clicked(object sender, EventArgs e)
         {
             Crashes.GenerateTestCrash();
+        }
+
+        private void Guess_Button_Clicked(object sender, EventArgs e)
+        {
+            int guess = int.Parse(Guess.Text);
+
+            if (guess == _number)
+            {
+                DisplayAlert("Success", "You got it!", "OK");
+                Submit.IsEnabled = false;
+                NewGame.IsEnabled = true;
+            }
+            else
+            {
+                DisplayAlert("Oops", "Not this time! Please try again!", "OK");
+                Guess.Text = "";
+                Guess.Focus();
+            }
+        }
+
+        private void NewGame_Button_Clicked(object sender, EventArgs e)
+        {
+            _number = 1 + rnd.Next(99);
+            Submit.IsEnabled = true;
+            Guess.Text = "";
+            Guess.Focus();
+            NewGame.IsEnabled = false;
         }
     }
 }
